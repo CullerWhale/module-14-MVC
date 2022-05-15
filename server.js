c96295453fdeb434edb83a1784d017cfaceaf965
CullerWhale <jcullenharris@gmail.com>
2022-05-15 15:17:24 -0500

    handlebar connected

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+
+//handlebars 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,10 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on ' + PORT));
+  app.listen(PORT, () => console.log('Now listening on http://localhost:' + PORT));
 });
